8aaafc46eec3e4930049072a5270e7c8af66555f
Sanchit <sanchits174@gmail.com>
2018-06-14 10:52:45 -0700

    Added getMonthName method
    Added the function for displaying current month name.

--- src/App.js
+++ src/App.js
@@ -4,15 +4,26 @@ import React, { Component } from 'react';
 let header = {
     backgroundColor: '#222'
 }
-let defaultTextColor = '#fff';
+let defaultTextColor = {
+    color: '#fff'
+} 
 
 
 class App extends Component {
+        const getMonthName = (number) => {
+            let monthArray = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'August', 'Sep', 'October', 'Nov', 'Dec'];
+
+            return monthArray[number];
+        
+    }
+        
   render() {
+//            Get curreny date
+            let today = new Date().getMonth();
     return (
-        <div style={{...header, height: '200px'}}>
-      <div style={{color: defaultTextColor}}>Just the initial file</div>
-        </div>
+        <div style={{...header, ...defaultTextColor, height: '200px'}}>
+            The month is {this.getMonthName(today)}
+        </div>              
     );
   }
 }
