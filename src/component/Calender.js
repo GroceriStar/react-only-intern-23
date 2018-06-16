a6749a170fda496531d6d07a222e551ffce1c093
Sanchit <sanchits174@gmail.com>
2018-06-16 12:09:58 -0700

    Rendered current month's calender
    Calender component renders current month's calender.

--- src/component/Calender.js
+++ src/component/Calender.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
 import * as styleVars from './style_vars';
+
+
 class Calender extends Component {
     render() {
-         let days = {0: 'Sun', 1: 'Mon', 2: 'Tue', 3: 'Wed', 4: 'Thu', 5: 'Fri', 6: 'Sat'};
-
+        let days = {0: 'Sun', 1: 'Mon', 2: 'Tue', 3: 'Wed', 4: 'Thu', 5: 'Fri', 6: 'Sat'};
+        let today = new Date();
+        today.setDate(1);
+        let dayOfWeek = today.getDay();
+        var count = 0;
+        
         return(
             <table>
-                <thead>
                     <tr>
                         {Object.keys(days).map((index) => <th key={index} style={{...styleVars.defaultTextColor, ...styleVars.blockSize, backgroundColor: '#232'}}>{days[index]}</th>)}
                     </tr>
                     <tr>
-                        {}
+                        {Object.keys(days).map(index => {
+                                                if(index < dayOfWeek) {
+                                                    return <td key={index} style={{...styleVars.defaultTextColor, ...styleVars.blockSize, backgroundColor: '#bbb'}}></td>}
+                                                else {count++;
+                                                      return <td key={index} style={{...styleVars.defaultTextColor, ...styleVars.blockSize, backgroundColor: '#777'}}>{count}</td>}
+                                                })}
                     </tr>
-                </thead>
+                    <tr>
+                    {Object.keys(days).map(index => {count++;
+                                                     return <td key={7+index} style={{...styleVars.defaultTextColor, ...styleVars.blockSize, backgroundColor: '#777'}}>{count}</td>})}
+                    </tr>
+                    <tr>
+                    {Object.keys(days).map(index => {count++;
+                                                     return <td key={14+index} style={{...styleVars.defaultTextColor, ...styleVars.blockSize, backgroundColor: '#777'}}>{count}</td>})}
+                    </tr>
+                    <tr>
+                    {Object.keys(days).map(index => {count++;
+                                                     return <td key={21+index} style={{...styleVars.defaultTextColor, ...styleVars.blockSize, backgroundColor: '#777'}}>{count}</td>})}
+                    </tr>
+                    <tr>
+                    {Object.keys(days).map(index => {count++;
+                                                     return <td key={7+index} style={{...styleVars.defaultTextColor, ...styleVars.blockSize, backgroundColor: '#777'}}>{count}</td>})}
+                    </tr>
+                
             </table>
             
         )
