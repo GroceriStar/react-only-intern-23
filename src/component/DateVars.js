7271daf09f2e9384c94a0ff6d89707c2217bc05a
Sanchit <sanchits174@gmail.com>
2018-06-17 10:57:47 -0700

    Modified DayChecker to use callback functions

--- src/component/DateVars.js
+++ src/component/DateVars.js
@@ -2,7 +2,7 @@ let days = {0: 'Sun', 1: 'Mon', 2: 'Tue', 3: 'Wed', 4: 'Thu', 5: 'Fri', 6: 'Sat'
 let maxDaysOfMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];  //This is the array of maximum number of days in each month.
 let today = new Date();
 let displayDate = today.toDateString();
-today.setDate(1);
+today.setDate(1);  //setting current month's date to 1 to get which day of week it is.
 let dayOfWeek = today.getDay();
 
 
--- src/component/DayChecker.js
+++ src/component/DayChecker.js
@@ -2,29 +2,51 @@ import React, { Component } from 'react';
 import * as styleVars from './style_vars';
 import { Header } from './Header'
 
+let today = new Date().getDay();
 
 class DayChecker extends Component {
+    constructor(props) {
+        super(props);
+        this.weekCheck = this.weekCheck.bind(this);
+    }
+    
     getDayName(number) {
     let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     return days[number];
       }
+    
+    weekCheck(dayNum, callback, otherCallback) {
+    if (dayNum === 6 || dayNum === 0) {
+        return callback();
+    }
+    else {
+        return otherCallback();
+    }
+}
+
+    weekend = () => {return (<div style={{...styleVars.header, ...styleVars.defaultTextColor, height: '200px'}}>
+                              <Header />
+                               <h3>Today is {this.getDayName(today)} - You can watch TV all day today</h3>
+                          </div>);
+                        }
+
+    weekday = () => {return (<div style={{...styleVars.header, ...styleVars.defaultTextColor, height: '200px'}}>
+                                <Header />
+                                <h3>Today is {this.getDayName(today)} - You should go to work today</h3>
+                            </div>);
+                        }
+
     render() {
-            let today = new Date().getDay();
-            // If it is weekday
-            if (today === 6 || today === 0) 
-              return (<div style={{...styleVars.header, ...styleVars.defaultTextColor, height: '200px'}}>
-                          <Header />
-                           <h3>Today is {this.getDayName(today)} - You can watch TV all day today</h3>
-                      </div>);
             
-            // If it is weeken
-            return (<div style={{...styleVars.header, ...styleVars.defaultTextColor, height: '200px'}}>
-                            <Header />
-                            <h3>Today is {this.getDayName(today)} - You should go to work today</h3>
-                    </div>);
+        return(<div>
+            {this.weekCheck(today, this.weekend, this.weekday)}
+              </div>);
           }    
 
 }
 
 
 export { DayChecker }
+
+
+
