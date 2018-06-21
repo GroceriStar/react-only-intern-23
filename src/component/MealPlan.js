a31fb0969af5b31ddd6b7261f2f461ef234b0b30
Sanchit <sanchits174@gmail.com>
2018-06-21 12:38:46 -0700

    Improved MealPlan.js

--- src/component/MealPlan.js
+++ src/component/MealPlan.js
@@ -8,17 +8,32 @@ class MealPlan extends Component {
             meals: []
         };
         this.renderMeals = this.renderMeals.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+        this.changeHandle = this.changeHandle.bind(this);
     }
     
     renderMeals() {
         return (this.state.meals.map((meal, index) => <li key={index}>{meal}</li>));
     }
+    
+    changeHandle(event) {
+                this.setState({
+                meal: event.target.value
+                });                
+    }
+    
+    handleClick() {
+        this.setState({
+            meals: [...this.state.meals, this.state.meal]
+        })
+    }
+    
     render() {
         return(
         <div>
             <h2>Today you should eat this</h2>
-            <input onChange={e => this.setState({meal: e.target.value})} value={this.state.meal} type='text'/>
-            <button onClick={() => this.setState({meals: [...this.state.meals, this.state.meal]})}>Add meal</button>
+            <input onChange={this.changeHandle} value={this.state.meal} type='text'/>
+            <button onClick={this.handleClick}>Add meal</button>
             <ul>
                 {this.renderMeals()}
             </ul>
