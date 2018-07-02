ef00eda6962857b378adfcc09c44467e330ac68c
sanchit94 <sanchits174@gmail.com>
2018-07-02 15:43:53 +0530

    Moved meal card into seperate component, also merged various arrays into an array of objects in MealPlan. Fixes #42 and #43

---
+++ src/component/MealPlanner/Cards.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {
+  Container, Row, Col, Card, CardImg, CardText, CardBody, CardTitle,
+} from 'reactstrap';
+import { cardSize, cardHeight, topMargin } from './MealPlanStyle';
+
+
+class MealCard extends Component {
+    render() {
+        return(
+        <div>
+                        <Col xs='3'>
+                          <Card style={cardSize}>
+                            <CardImg top width="100%" style={cardHeight} src={this.props.mealprop.image} alt="This image is not supported" />
+                            <CardBody>
+                              <CardTitle>{this.props.mealprop.name}</CardTitle>
+                              <CardText>{this.props.mealprop.description}</CardText>
+                            </CardBody>
+                          </Card>
+                        </Col>
+        </div>
+        )
+    }
+}
+
+export { MealCard }
---
+++ src/data/MealData.js
@@ -0,0 +1,5 @@
+import { img_url } from './Images_url';
+
+const mealInit = [{name: 'Burger', image: img_url[0], description: 'Burgers are eaten all over the world, and are one of the most handy and easy to make recipy' }, {name: 'Pizza', image: img_url[1], description: 'Pizza is a mouth watering dish'}];
+
+export { mealInit }
