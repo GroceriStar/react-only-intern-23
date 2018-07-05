e4b05b50cf2ea4a5b367c15bb024969fe7ea2834
Sanchit <sanchits174@gmail.com>
2018-07-05 09:13:08 -0700

    Revert "Test env" Undid last two commits where I instlled jest, and enzyme-preset-react-2015" This reverts commit d2f12729d284f3e9823cdaf27d2b0a36aea8fac9.

--- src/data/MealData.js
+++ src/data/MealData.js
@@ -1,17 +1,5 @@
 import { img_url } from './Images_url';
 
-const mealInit = [
-            {
-             name: 'Burger',
-             image: 'https://dmi4pvc5gbhhd.cloudfront.net/2016/08/easy-black-bean-burger-4-650x433.jpg', 
-             description: 'Burgers are eaten all over the world, and are one of the most handy and easy to make recipy' 
-            }, 
-            {
-             name: 'Pizza',
-             image: 'https://recipes.timesofindia.com/photo/53110049.cms',
-             description: 'Pizza is a mouth watering dish'
-            }
-];
-            
+const mealInit = [{name: 'Burger', image: img_url[0], description: 'Burgers are eaten all over the world, and are one of the most handy and easy to make recipy' }, {name: 'Pizza', image: img_url[1], description: 'Pizza is a mouth watering dish'}];
 
 export { mealInit }
