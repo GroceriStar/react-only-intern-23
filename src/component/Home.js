6137a3a2dd148b29428b29fabfec2c4e6685661e
Sanchit <sanchits174@gmail.com>
2018-07-18 16:15:01 -0700

    Created prototype recipy form using react-final-form

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Calendar }  from './Calendar/Calendar';
 import { MealPlan }  from './MealPlanner/MealPlan';
+import { Recipy } from './Recipy/RecipyForm'
 
 class Home extends Component {
     render() {
@@ -8,6 +9,7 @@ class Home extends Component {
             <div>
             <Calendar />
             <MealPlan />
+            <Recipy />
             </div>
         );
     }
