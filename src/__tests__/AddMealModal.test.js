3938f5d7f7fc946b2b42a33dae1ac44c7efb7d67
Sanchit <sanchits174@gmail.com>
2018-07-29 15:00:30 -0700

    Fixed failing tests

--- src/__tests__/AddMealModal.test.js
+++ src/__tests__/AddMealModal.test.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme, {shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import App from '../Modals/AddMealModal';
+import { AddMeal } from '../component/Modals/AddMealModal';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<AddMealModal />', () => {
     it('renders without crashing', () => {
         const div = document.createElement('div');
-        ReactDOM.render(<AddMealModal />, div);
+        ReactDOM.render(<AddMeal />, div);
     });
 });
--- src/__tests__/Attribute.test.js
+++ src/__tests__/Attribute.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme, {shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import App from '../FormFields/Attribute';
+import { Attribute } from '../component/FormFields/Attribute';
 
 Enzyme.configure({ adapter: new Adapter() });
 
