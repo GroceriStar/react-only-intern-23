74f2e4acdacd6ad150ed3c1123408fb663d30e32
sanchit94 <sanchits174@gmail.com>
2018-07-31 22:59:04 +0530

    Fixed failing test for RecipeForm

--- src/__tests__/RecipeForm.test.js
+++ src/__tests__/RecipeForm.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Recipe } from '../component/Recipe/RecipeForm';
+import { Recipes } from '../component/Recipe/RecipeForm';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 it('renders correctly', () => {
-    shallow(<Recipe/>);
+    shallow(<Recipes/>);
 });
