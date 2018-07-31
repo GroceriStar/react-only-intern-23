4a1457c0d981bbb65d6cb57a11ebad3040c00feb
sanchit94 <sanchits174@gmail.com>
2018-07-31 19:56:27 +0530

    Minor fix

--- src/__tests__/Attribute.test.js
+++ src/__tests__/Attribute.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Enzyme, {shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { Attribute } from '../component/FormFields/Attribute';
+import { Attribute } from '../component/MealPlanner/FormFields/Attribute';
 
 Enzyme.configure({ adapter: new Adapter() });
 
